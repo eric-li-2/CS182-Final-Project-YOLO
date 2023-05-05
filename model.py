29f1bb55150d5bb9d252fc64dc3b09097826ac96
Nishant <nishant@bhakars.com>
2023-05-04 21:34:42 -0700

    last change

--- model.py
+++ model.py
@@ -106,7 +106,6 @@ class Yolov1(nn.Module):
         return nn.Sequential(
             nn.Flatten(),
             nn.Linear(1024 * S * S, 496),
-            # nn.Dropout(0.0),
             nn.LeakyReLU(0.1),
             nn.Linear(496, S * S * (C + B * 5)),
         )
